c26faa6901f948fd3bbdf36eca0e0d4a1ee82d79
Leonardo Salazar <143781497+salazarleo@users.noreply.github.com>
2023-10-18 13:11:30 -0300

    Delete orderitem.py

--- orderitem.py
+++
@@ -1,7 +0,0 @@
-class Orderitem():
-    def __init__(self, quantity, unitary_price, order, product):
-        self.quantity = quantity
-        self.unitary_price = unitary_price
-        self.order = order
-        self.product = product
-        
