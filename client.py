384478e79ff2d0124f55479c276efd31a6c3f049
Leonardo Salazar <143781497+salazarleo@users.noreply.github.com>
2023-10-18 13:11:43 -0300

    Delete client.py

--- client.py
+++
@@ -1,9 +0,0 @@
-class Client():
-    
-    def __init__(self, first_name, last_name, addres, cell_phone, email, gender):
-        self.first_name = first_name
-        self.last_name = last_name
-        self.addres = addres
-        self.cell_phone = cell_phone
-        self.email = email
-        self.gender = gender
